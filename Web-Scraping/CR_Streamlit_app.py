b9f3e288d72fd884fe7ae763db8e60507f5fd4de
ceasler <ceasler11@icloud.com>
2025-11-12 16:34:30 -0500

    Add to repo to test functionality of startup

---
+++ Web-Scraping/CR_Streamlit_app.py
@@ -0,0 +1,6 @@
+#I decided to use streamlit to create a small model website to display our data
+import streamlit as st
+from datetime import datetime
+
+st.set_page_config(page_title="Simple Crypto Rate App", layout="centered")
+st.write("Hello World")
