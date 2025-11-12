67fe5c78e2b54d084cf88ee84c326117fb306b58
ceasler <ceasler11@icloud.com>
2025-11-12 16:34:36 -0500

    Add to repo to test functionality of startup

--- Web-Scraping/CryptoRates.py
+++ Web-Scraping/CryptoRates.py
@@ -1,3 +1,5 @@
+
+
 from bs4 import BeautifulSoup as bs
 import requests
 import time
@@ -65,9 +67,3 @@ if not found:
 print('--- code finished in %.3f seconds ---' % (time.time() - start_time))
 
 
-#I decided to use streamlit to create a small model website to display our data
-"""import streamlit as st
-from datetime import datetime
-
-st.set_page_config(page_title="Simple Crypto Rate App", layout="centered")
-st.write("Hello World")"""
